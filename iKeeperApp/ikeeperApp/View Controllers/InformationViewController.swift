127073392c6f7974eb1a0e1e348756ee3efed52c
hyeeejin2 <gpwlwlgml2@gmail.com>
2020-11-22 19:17:10 +0900

    20201122

--- iKeeperApp/ikeeperApp/View Controllers/InformationViewController.swift
+++ iKeeperApp/ikeeperApp/View Controllers/InformationViewController.swift
@@ -18,23 +18,29 @@ class InformationViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
-        //tableView.register(InfoCustomCell.self, forCellReuseIdentifier: "InfoCustomCell")
-        //tableView.estimatedRowHeight = 200 // 임의 설정
-        //tableView.rowHeight = UITableView.automaticDimension // autolayout 설정에 맞게 자동으로 table vell height 조절
+        tableView.estimatedRowHeight = 80.0 // 임의 설정이지만 계산해서 80
+        tableView.rowHeight = UITableView.automaticDimension // autolayout 설정에 맞게 자동으로 table cell height 조절
         
         dataAdd()
     }
     
     func dataAdd() {
-        let data1: [String: String] = ["num": "1", "title": "sw개발보안", "views": "11", "date": "2020-11-20", "time": "14:00", "writer": "김혜진"]
+        let data1: [String: String] = ["num": "1", "title": "sw개발보안경진대회", "views": "11",
+                                       "date": "2020-11-20", "time": "14:00", "writer": "김혜진"]
         dataList.append(data1)
-        let data2: [String: String] = ["num": "2", "title": "kucis 영남권 세미나", "views": "30", "date": "2020-12-12", "time": "13:00", "writer": "김혜진"]
+        let data2: [String: String] = ["num": "2", "title": "kucis 영남권 세미나", "views": "30",
+                                       "date": "2020-12-12", "time": "13:00", "writer": "김혜진"]
         dataList.append(data2)
     }
 
 }
 
 extension InformationViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // tableView setting - height of row
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
     
     // tableView setting - row
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -47,7 +53,7 @@ extension InformationViewController: UITableViewDelegate, UITableViewDataSource 
         
         //cell은 as 키워드로 앞서 만든 InfoCustomCell 클래스화
         let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCustomCell", for: indexPath) as! InfoCustomCell
-        
+
         // cell에 데이터 삽입
         let data = dataList[indexPath.row]
         cell.numLabel?.text = data["num"]
@@ -58,13 +64,13 @@ extension InformationViewController: UITableViewDelegate, UITableViewDataSource 
         cell.writerLabel?.text = data["writer"]
         return cell
     }
-/*
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        print("select")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
- */
 }
