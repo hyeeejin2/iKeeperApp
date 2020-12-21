1cd9163c695ff09d8cfcd13fb128b3ac610963ef
hyeeejin2 <gpwlwlgml2@gmail.com>
2020-12-21 16:35:14 +0900

    20201221

--- iKeeperApp/ikeeperApp/View Controllers/InformationWriteViewController.swift
+++ iKeeperApp/ikeeperApp/View Controllers/InformationWriteViewController.swift
@@ -6,24 +6,63 @@
 //
 
 import UIKit
+import Firebase
 
 class InformationWriteViewController: UIViewController {
 
+    @IBOutlet weak var titleValue: UITextField!
+    @IBOutlet weak var writerValue: UITextField!
+    @IBOutlet weak var dateValue: UITextField!
+    @IBOutlet weak var timeValue: UITextField!
+    @IBOutlet weak var contentValue: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func writeButton(_ sender: UIButton) {
+        guard let title: String = titleValue.text, title.isEmpty == false else {
+            showAlert(message: "빈칸을 채워주세요")
+            return
+        }
+        guard let writer: String = writerValue.text, writer.isEmpty == false else {
+            showAlert(message: "빈칸을 채워주세요")
+            return
+        }
+        guard let date: String = dateValue.text, date.isEmpty == false else {
+            showAlert(message: "빈칸을 채워주세요")
+            return
+        }
+        guard let time: String = timeValue.text, time.isEmpty == false else {
+            showAlert(message: "빈칸을 채워주세요")
+            return
+        }
+        guard let content: String = contentValue.text, content.isEmpty == false else {
+            showAlert(message: "빈칸을 채워주세요")
+            return
+        }
+        
+        let views: Int = 0
+        let db = Firestore.firestore()
+        let newDocument = db.collection("infoList").document()
+        newDocument.setData(["id": newDocument.documentID,"title": title, "writer": writer, "date": date, "time": time, "views": views, "content": content]) { (error) in
+            if error != nil {
+                print("check for error : \(error!.localizedDescription)")
+                self.showAlert(message: "게시글 등록 실패")
+            } else {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
     }
-    */
 
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "알림",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
